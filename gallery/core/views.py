1acb162b24a568b2274bb674f971304761fd35ca
rg3915 <rg3915@yahoo.com.br>
2016-11-08 17:33:28 -0200

    views

--- gallery/core/views.py
+++ gallery/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy as r
 from .models import Gallery
+from .forms import GalleryForm
 
 
 def home(request):
@@ -9,12 +10,13 @@ def home(request):
 
 
 class GalleryList(ListView):
-    template_name = 'gallery_list.html'
+    template_name = 'core/gallery_list.html'
     model = Gallery
     context_object_name = 'gallery'
 
 
 class GalleryCreate(CreateView):
-    template_name = 'gallery_create_form.html'
+    template_name = 'core/gallery_form.html'
     model = Gallery
-    success_url = reverse_lazy('gallery_list')
+    form_class = GalleryForm
+    success_url = r('gallery_list')
