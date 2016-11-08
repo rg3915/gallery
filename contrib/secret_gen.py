4a0a2bfa17e8e434889a054afdef4275c486054f
rg3915 <rg3915@yahoo.com.br>
2016-11-08 17:32:47 -0200

    contrib

---
+++ contrib/secret_gen.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Django SECRET_KEY generator.
+"""
+from django.utils.crypto import get_random_string
+
+
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+print(get_random_string(50, chars))
+
+# hbn.link/secret_gen
+
