005f53ea2dd564357fe400b7a0a2bb572eb0e7a9
rg3915 <rg3915@yahoo.com.br>
2016-11-08 23:28:23 -0200

    Timestamped

--- gallery/core/models.py
+++ gallery/core/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 
-class Gallery(models.Model):
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(
+        'criado em', auto_now_add=True, auto_now=False)
+    modified = models.DateTimeField(
+        'modificado em', auto_now_add=False, auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Gallery(TimeStampedModel):
     description = models.CharField(
         'descrição', max_length=20, null=True, blank=True)
     photo = models.ImageField(upload_to='media')
 
     class Meta:
-        ordering = ['description']
+        ordering = ['-created']
         verbose_name = 'foto'
         verbose_name_plural = 'fotos'
 
